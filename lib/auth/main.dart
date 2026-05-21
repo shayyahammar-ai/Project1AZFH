94e111c342525125fcd6ad472b02e4b9c07f11b1
fmaalzahraa-dot <fmaalzahraa@gmail.com>
2026-05-21 13:14:24 +0300

    login and regester done

---
+++ lib/auth/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:project_azfh/auth/SplashScreen.dart';
+
+
+void main() {
+  runApp(
+     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    ),
+  );
+}
